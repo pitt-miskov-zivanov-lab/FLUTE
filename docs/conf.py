82c79bae4ef00b8415f8d817e3b290157ea2996b
Emilee Holtzapple <erh87@pitt.edu>
2022-05-04 01:17:12 +0000

    conf.py edited online with Bitbucket
    Former-commit-id: 20df140aea0d9f8bbaadcbea2765e12357ae5070

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-import mock
+
 import run_FLUTE
 
-MOCK_MODULES = ['mysql']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+autodoc_mock_imports = ['mysql',"mysqlclient","mysql.connector"]
 
 # conf.py options for Latex
 latex_engine = 'pdflatex'
