7aa56d9a407f06691d443f0ea2d73d94f6c252f7
gavinchou94 <gaz11@pitt.edu>
2024-05-29 21:42:47 -0400

    update rst

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'rst2pdf.pdfbuilder',
               'sphinx.ext.extlinks',
-              'nbsphinx']
+              'nbsphinx',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/notebook.rst
+++ docs/notebook.rst
@@ -3,4 +3,7 @@ Notebook Tutorial
 
 This file goes through the functions in FLUTE tool in an elaborative and interactive manner using notebook.
 
-.. nbsphinx:: use_FLUTE
+.. toctree::
+    :maxdepth: 2
+
+    use_FLUTE.ipynb
